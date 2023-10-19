462c1b331fb1ce2000f5eb88bc8b34338e3e475b
ETKoenig <144128984+ETKoenig@users.noreply.github.com>
2023-10-19 07:56:35 -0400

    Update HW1.cpp

--- HW1.cpp
+++ HW1.cpp
@@ -1,1 +1,35 @@
-//test 
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+
+//1)write a program that prints "Hello Toop!"
+cout << "Hello Toop!";
+
+//2) declare variables
+string name;
+int age;
+double decimalAge;
+double months;
+
+//3) request name and store in name
+cout << "Please enter your full name (first and last): "; 
+getline (cin, name);
+cout << " Thank you, Please enter your age in whole years: "; 
+cin >> age;
+cout << "Thank you, Please enter the number of months since your birthday: ";
+cin >> months;
+decimalAge = double (age) + (months/12);
+
+//4) output response
+
+cout << "Your name is " << name <<", and you are " << decimalAge << " years old!";
+
+ofstream outfile;
+outfile.open("TOOPHW1.txt", std::ofstream::app);
+outfile << name << " : " << decimalAge <<endl;
+outfile.close();
+
+ 
